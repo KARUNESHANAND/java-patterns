effea37cafe7429efd146c0987e1df2dd10f87d5
Karunesh Anand <83410805+KARUNESHANAND@users.noreply.github.com>
2023-08-07 07:23:40 +0530

    Create Pattern9.java

---
+++ Pattern9.java
@@ -0,0 +1,23 @@
+class Pattern9 {
+
+    void printDiamond(int n) {
+        for(int i=1; i<=n ; i++) {
+            for(int j=0; j <(n-i); j++) {
+                System.out.print(" ");
+            }
+            for(int j=0; j<i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=0; i<n ; i++) {
+            for(int j=0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for(int j=0; j<(n-i);j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
